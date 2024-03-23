e49c4873c633a2a5b4a793e82341f30483ee7146
Paul Davies <lagdotcom@gmail.com>
2024-03-23 18:00:06 +0000

    change mailing list to hash of email:name

--- api/signup.ts
+++ api/signup.ts
@@ -1,11 +1,19 @@
 import { kv } from '@vercel/kv';
 
+const emailPattern = /^[\w\-\.]+@([\w-]+\.)+[\w-]{2,}$/;
+const mailingListKey = 'mailing-list';
+
 export async function GET(req: Request) {
   const url = new URL(req.url);
+
+  const name = url.searchParams.get('name');
+  if (!name) return new Response('Missing ?name', { status: 400 });
+
   const email = url.searchParams.get('email')
   if (!email) return new Response('Missing ?email', { status: 400 });
+  if (!emailPattern.test(email)) return new Response('Invalid ?email', { status: 400 });
 
-  await kv.sadd('mailing-list', email);
+  await kv.hset(mailingListKey, { email: name })
 
-  return new Response(undefined, { status: 204 });
+  return new Response('OK');
 }
